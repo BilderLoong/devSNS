e5ae8654d936cb7b2e49467324ba2ef987166ccf
bilder <514059172@163.com>
2021-03-11 20:19:00 +0800

    get all post & get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -40,4 +40,33 @@ router.get(
   }
 );
 
+// @route    POST api/posts
+// @desc     Get all posts
+// @access   Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    POST api/posts/:id
+// @desc     Get post by ID
+// @access   Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const posts = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ msg: 'Post not found' });
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kine === 'ObjectId') {
+      if (!post) return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
