806e2b23ef8ed94b6f54d7018883a2e080d6c5fa
bilder <514059172@163.com>
2021-03-12 10:27:10 +0800

    remove post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -173,4 +173,35 @@ router.post(
   }
 );
 
+// @route    POST api/posts/comment/:post_id/:comment_id
+// @desc     Delete a post
+// @access   Private
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorize' });
+    }
+
+    // Remove the comment
+    post.comments = post.comments.filter(
+      (currentCom) => currentCom.id !== comment.id
+    );
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
