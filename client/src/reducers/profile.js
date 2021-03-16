e42848b487d13768bf682c5537ecb00b03599da9
bilder <514059172@163.com>
2021-03-16 11:59:44 +0800

    add AddEducation and AddExperience components

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,9 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+import {
+  UPDATE_PROFILE,
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+} from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -13,8 +18,8 @@ export default (state = initialState, action) => {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return { ...state, profile: payload, loading: false };
-
     case PROFILE_ERROR:
       return { ...state, error: payload, loading: false };
 
