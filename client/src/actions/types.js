85e904e5c601efcc761efbb11f6031c407b6999f
bilder <514059172@163.com>
2021-03-16 15:33:02 +0800

    add action: deleteExperience, deleteEducation, deleteAccout

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,6 +4,7 @@ export const REMOVE_ALERT = 'REMOVE_ALERT';
 
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
