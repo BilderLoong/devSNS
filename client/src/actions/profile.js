b9d0fae333e391ccdef553edd918bb602d8d690e
bilder <514059172@163.com>
2021-03-16 19:03:32 +0800

    add actions: getProfiles getProfileById  getGithubRepos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,9 +5,11 @@ import { setAlert } from './alert';
 import {
   UPDATE_PROFILE,
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   CLEAR_PROFILE,
   ACCOUNT_DELETED,
+  GET_REPOS,
 } from './types';
 
 export const getCurrentProfile = () => async (dispatch) => {
@@ -22,6 +24,48 @@ export const getCurrentProfile = () => async (dispatch) => {
   }
 };
 
+// Get all profiles
+export const getProfiles = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
+  try {
+    const res = await axios.get('/api/profile');
+
+    dispatch({ type: GET_PROFILES, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get profile by user Id
+export const getProfileById = (userId) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/profile/${userId}`);
+
+    dispatch({ type: GET_PROFILE, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get Github repos
+export const getGithubRepos = (username) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+
+    dispatch({ type: GET_REPOS, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 //Create or update profile
 export const createProfile = (formData, history, edit = false) => async (
   dispatch
@@ -86,6 +130,7 @@ export const addEducation = (formData, history) => async (dispatch) => {
     dispatch({ type: UPDATE_PROFILE, payload: res.data });
 
     dispatch(setAlert('Education Added', 'success'));
+    history.push('/dashboard');
   } catch (err) {
     const errors = err.response.data.errors;
     if (errors)
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,6 +3,8 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
 } from '../actions/types';
 
 const initialState = {
@@ -20,11 +22,15 @@ export default (state = initialState, action) => {
     case GET_PROFILE:
     case UPDATE_PROFILE:
       return { ...state, profile: payload, loading: false };
+    case GET_PROFILES:
+      return { ...state, loading: false, profiles: payload };
     case PROFILE_ERROR:
       return { ...state, error: payload, loading: false };
-
     case CLEAR_PROFILE:
       return { ...state, profile: null, repos: [], loading: false };
+
+    case GET_REPOS:
+      return { ...state, repos: payload, loading: false };
     default:
       return state;
   }
