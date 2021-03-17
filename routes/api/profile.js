42bb8edbd2d6ea46d2bc33cb10101c39c55b17ec
bilder <514059172@163.com>
2021-03-17 10:46:13 +0800

    change the wrong variable name and unadjut http reponse code

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -125,19 +125,19 @@ router.get('/', async (req, res) => {
 // @ access Public
 router.get('/:user_id', async (req, res) => {
   try {
-    const profiles = await Profile.findOne({
+    const profile = await Profile.findOne({
       user: req.params.user_id,
     }).populate('user', ['name', 'avatar']);
 
-    if (!profiles) {
-      return res.status(400).json({ msg: 'Profile not found' });
+    if (!profile) {
+      return res.status(404).json({ msg: 'Profile not found' });
     }
 
-    res.json(profiles);
+    res.json(profile);
   } catch (err) {
     console.error(err);
     if (err.kind == 'ObjectId') {
-      return res.status(400).json({ msg: 'Profile not found' });
+      return res.status(404).json({ msg: 'Profile not found' });
     }
     res.status(500).send('Server Error');
   }
