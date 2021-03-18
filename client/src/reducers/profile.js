0cdf142e1c04974135377dbe1a9de5d9893260e2
bilder <514059172@163.com>
2021-03-18 10:07:39 +0800

    fix a security issue

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -25,7 +25,7 @@ export default (state = initialState, action) => {
     case GET_PROFILES:
       return { ...state, loading: false, profiles: payload, error: {} };
     case PROFILE_ERROR:
-      return { ...state, error: payload, loading: false };
+      return { ...state, error: payload, loading: false, profile: null };
     case CLEAR_PROFILE:
       return { ...state, profile: null, repos: [], loading: false, error: {} };
 
